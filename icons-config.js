bd65ec2624ece6d7bfa3d92995d07481daeb29b4
Parvares <55709458+Parvares@users.noreply.github.com>
2025-02-05 12:48:16 +0100

    Update icons-config.js

--- icons-config.js
+++ icons-config.js
@@ -149,7 +149,14 @@ municipio1: L.icon({
     shadowSize: [50, 50] // Adattata l'ombra
         }),
 
-
+    link: L.icon({
+            iconUrl: 'univiola.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [50, 50], // Aumentata larghezza e altezza
+    iconAnchor: [25, 50], // Centrato il punto di ancoraggio
+    popupAnchor: [1, -40], // Leggermente adattato
+    shadowSize: [50, 50] // Adattata l'ombra
+        }),
     
     
         castelli: L.icon({
